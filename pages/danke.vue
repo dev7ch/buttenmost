4f936097a930586aa33fdbdf2f1230bab6f50044
shufschmid <shufschmid@gmail.com>
2021-04-02 23:53:32 +0200

    fixed image width/position

---
+++ pages/danke.vue
@@ -0,0 +1,17 @@
+<template>
+<v-container>
+  <v-row class="text-center">
+    <v-col>
+<img src="../static/family.jpg" style="max-width: 600px;"/>
+      <blockquote class="blockquote" >
+        &#8220;Danke für Ihre Bestellung.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;Familie Vögtli/Zaugg/Ming</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</v-container>
+</template>
