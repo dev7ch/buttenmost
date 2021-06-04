cb7051d1f24599f578ba38e983840894887df9ac
Samuel Hufschmid <shufschmid@gmail.com>
2021-06-04 17:20:12 +0200

    set up webhook-boilerplate using netlify cli

---
+++ functions/webhook/payrexx.js
@@ -0,0 +1,10 @@
+exports.handler = async (event, context) => {
+  try {
+    console.log(event.body)
+    return {
+      statusCode: 200
+    }
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() }
+  }
+}
