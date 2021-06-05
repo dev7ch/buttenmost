64004e0d3629705eb6a8044bca85e9e955dfc2bf
Samuel Hufschmid <shufschmid@gmail.com>
2021-06-05 14:09:37 +0200

    added sandbox-domain for mailgun

--- functions/sendmail.js
+++ functions/sendmail.js
@@ -3,12 +3,12 @@ const mailgun = require("mailgun-js");
 exports.handler = function(event, context, callback) {
   const mg = mailgun({
     apiKey: "aa9f95815d1cf88b900a55121d5cd0d8-1d8af1f4-e5d57eb1",
-    domain: "buttenmost.ch"
+    domain: "sandboxd86c42804fa142eaa48ce3644961f3c2.mailgun.org"
   });
 
   const data = {
-    from: "Samuel <info@buttenmost.ch>",
-    to: "shufschmid@gmail.com@musk.com",
+    from: "Samuel <info@sandboxd86c42804fa142eaa48ce3644961f3c2.mailgun.org>",
+    to: "shufschmid@gmail.com",
     subject: "Nur ein Test",
     text: "mal schauen, ob es klappt.",
     html: "HTML"
