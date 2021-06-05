e42d6c4f354d5a762e8fa11f98fd23a52a24562c
Samuel Hufschmid <shufschmid@gmail.com>
2021-06-05 15:03:13 +0200

    completely remove all api-keys also in older repo-versions

--- functions/sendmail.js
+++ functions/sendmail.js
@@ -1,8 +1,9 @@
 const mailgun = require("mailgun-js");
+const { MG_API_KEY: apiKey, MG_DOMAIN: domain } = process.env;
 exports.handler = function(event, context, callback) {
   const mg = mailgun({
-    apiKey: process.env.MAILCHIMP_API_KEY,
-    domain: "sandboxd86c42804fa142eaa48ce3644961f3c2.mailgun.org"
+    apiKey: apiKey,
+    domain: domain
   });
 
   const data = {
